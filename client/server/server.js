f160d112933d52823456ed9f44fc5ebf23fd4863
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-02-03 17:45:04 +0900

    서버 디렉터리 재설정
    서버 디렉터리 재설정후, client, server 연동 시켜줬음

--- client/server/server.js
+++ client/server/server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/api/host', (req, res) => {
+    res.send( {host: 'Hello World!'})
 })
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
